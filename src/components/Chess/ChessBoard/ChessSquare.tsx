ded092254fc41ef5d0bb6b10569dab784f1a2dbc
Soap <jonflinchum@gmail.com>
2025-03-22 18:20:11 -0400

    rudimentary chess pieces

--- src/components/Chess/ChessBoard/ChessSquare.tsx
+++ src/components/Chess/ChessBoard/ChessSquare.tsx
@@ -1,15 +1,54 @@
 import { useState, useEffect, useMemo } from 'react'
+import { PieceSymbol, Color } from 'chess.js';
 import { ChessBoardSquare } from '../types';
+import blackPawn from '../../../assets/blackPawn.png';
+import blackRook from '../../../assets/blackRook.png';
+import blackKnight from '../../../assets/blackKnight.png';
+import blackBishop from '../../../assets/blackBishop.png';
+import blackQueen from '../../../assets/blackQueen.png';
+import blackKing from '../../../assets/blackKing.png';
+import whitePawn from '../../../assets/whitePawn.png';
+import whiteRook from '../../../assets/whiteRook.png';
+import whiteKnight from '../../../assets/whiteKnight.png';
+import whiteBishop from '../../../assets/whiteBishop.png';
+import whiteQueen from '../../../assets/whiteQueen.png';
+import whiteKing from '../../../assets/whiteKing.png';
 
 interface ChessSquareProps {
   square: ChessBoardSquare,
   backgroundColor: 'white' | 'black'
 }
 
-const ChessSquare = ({ square, backgroundColor }: ChessSquareProps) => {
+const getPieceImage = (pieceType: PieceSymbol, pieceColor: Color) => {
+  if (pieceColor === 'b') {
+    switch (pieceType) {
+      case 'p': return blackPawn;
+      case 'r': return blackRook;
+      case 'n': return blackKnight;
+      case 'b': return blackBishop;
+      case 'q': return blackQueen;
+      case 'k': return blackKing;
+    }
+  } else {
+    switch (pieceType) {
+      case 'p': return whitePawn;
+      case 'r': return whiteRook;
+      case 'n': return whiteKnight;
+      case 'b': return whiteBishop;
+      case 'q': return whiteQueen;
+      case 'k': return whiteKing;
+    }
+  }
+}
 
+const ChessSquare = ({ square, backgroundColor }: ChessSquareProps) => {
   return (
     <div className={`chessSquare ${backgroundColor}ChessSquare`}>
+      {
+        square?.type && square?.color && (
+          <img src={getPieceImage(square?.type, square?.color)} className='chessPiece'/>
+        )
+      }
     </div>
   )
 }
