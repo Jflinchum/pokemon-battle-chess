521104a3f40056426b2e4615fb87a4b58169104b
Soap <jonflinchum@gmail.com>
2025-03-23 22:59:07 -0400

    better design for battle log

--- src/components/PokemonManager/PokemonBattleDisplay/PokemonBattleLog/PokemonBattleLog.tsx
+++ src/components/PokemonManager/PokemonBattleDisplay/PokemonBattleLog/PokemonBattleLog.tsx
@@ -1,10 +1,26 @@
 import { useRef, useEffect, useMemo } from "react";
 import { Protocol } from "@pkmn/protocol";
 import { LogFormatter } from "@pkmn/view";
+import StylizedText from "../../../common/StylizedText/StylizedText";
 import './PokemonBattleLog.css';
 
 interface PokemonBattleLogProps {
   battleHistory: string[];
+}
+
+const getClassnameFromBattleArg = (args: string[]) => {
+  console.log(args);
+  if (args[0] === 'turn') {
+    return 'turnLog';
+  }
+  return '';
+}
+
+const formatTextFromBattleArg = (text: string, args: string[]) => {
+  if (args[0] === 'turn') {
+    return text.replace(/\=/g, '');
+  }
+  return text;
 }
 
 const PokemonBattleLog = ({ battleHistory }: PokemonBattleLogProps) => {
@@ -22,15 +38,19 @@ const PokemonBattleLog = ({ battleHistory }: PokemonBattleLogProps) => {
     <div className='pokemonBattleLogContainer' ref={containerRef}>
       {
         battleHistory.map((log) => {
-          let totalLog: string[] = [];
+          let totalLog: { text: string, args: string[]  }[] = [];
           for (const { args, kwArgs } of Protocol.parse(log)) {
             const formattedText = formatter.formatText(args, kwArgs);
             if (formattedText) {
-              totalLog.push(formattedText);
+              totalLog.push({ text: formattedText, args: (args as string[]) });
             }
           }
-          return totalLog.map((formattedBattleText, index) => (
-            formattedBattleText && (<p key={index}>{formattedBattleText}</p>)
+          return totalLog.map(({ text, args }, index) => (
+            text && (
+              <p className={`textLog ${getClassnameFromBattleArg(args)}`} key={index}>
+                <StylizedText text={formatTextFromBattleArg(text, args)} />
+              </p>
+            )
           ));
         }
         )
---
+++ src/components/common/StylizedText/StylizedText.tsx
@@ -0,0 +1,21 @@
+import { Fragment } from "react";
+
+const StylizedText = ({ text }: { text: string }) => {
+  const boldParts = text.split(/\*\*(.+?)\*\*/);
+
+  return (
+    <span>
+      {
+        boldParts.map((part, index) => {
+          if (index % 2 === 1) {
+            return <strong key={index}>{part}</strong>;
+          } else {
+            return <Fragment key={index}>{part}</Fragment>
+          }
+        })
+      }
+    </span>
+  )
+}
+
+export default StylizedText;
