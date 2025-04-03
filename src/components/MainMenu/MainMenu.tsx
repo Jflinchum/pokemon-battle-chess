94796d667506d453419cacb3c588591660542fc6
Soap <jonflinchum@gmail.com>
2025-04-03 13:13:02 -0400

    uncommitted changes

--- src/components/MainMenu/MainMenu.tsx
+++ src/components/MainMenu/MainMenu.tsx
@@ -4,27 +4,10 @@ import { useUserState } from '../../context/UserStateContext';
 import './MainMenu.css';
 import { useEffect } from 'react';
 import { useModalState } from '../../context/ModalStateContext';
-import { Dex } from '@pkmn/sim';
 
 const MainMenu = () => {
   const { userState } = useUserState();
   const { dispatch } = useModalState();
-  useEffect(() => {
-    if (!Dex.formats.get('pokemonBattleChess')) {
-      Dex.formats.extend([{
-        name: 'pokemonbattlechess',
-        mod: 'gen9',
-        onBattleStart: () => {
-          console.log(this);
-          debugger;
-        },
-        onBegin: () => {
-          console.log(this);
-          debugger;
-        }
-      }]);
-    }
-  }, []);
 
   useEffect(() => {
     if (userState.name.length === 0) {
