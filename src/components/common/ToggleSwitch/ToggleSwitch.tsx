e403a7d837d65ab5aed6821136230c985cd01994
Soap <jonflinchum@gmail.com>
2025-10-20 15:20:42 -0400

    fix issue where toggle switch was not clickable

--- src/components/common/ToggleSwitch/ToggleSwitch.tsx
+++ src/components/common/ToggleSwitch/ToggleSwitch.tsx
@@ -3,17 +3,24 @@ import "./ToggleSwitch.css";
 
 export const ToggleSwitch = ({
   className,
-  id,
   ...props
 }: React.InputHTMLAttributes<HTMLInputElement>) => {
   return (
-    <label className="switch" htmlFor={id}>
-      <Input
-        className={`checkbox ${className || ""}`}
-        type="checkbox"
-        {...props}
-      />
-      <span className="slider round"></span>
-    </label>
+    <>
+      {/*
+        This label is being styled as the toggle switch and needs to capture click events to toggle the input. However, we should not add an htmlFor here in order to prevent clashes with
+        other label elements intended to be used for the input element.
+      */}
+      {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
+      <label className="switch">
+        <Input
+          className={`checkbox ${className || ""}`}
+          type="checkbox"
+          role="switch"
+          {...props}
+        />
+        <span className="slider round"></span>
+      </label>
+    </>
   );
 };
